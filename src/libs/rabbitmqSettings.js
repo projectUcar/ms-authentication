549da62acbc6563520a794a4a5b9131fa1fa296b
Andrey Hernandez <andreyhernandeztorres@gmail.com>
2024-08-06 22:36:47 -0500

    RabbitMQ Adjustment

--- src/libs/rabbitmqSettings.js
+++ src/libs/rabbitmqSettings.js
@@ -1,10 +1,10 @@
 import amqp from 'amqplib/callback_api';
-import { PORT_RABBITMQ, USERNAME_RABBITMQ, PASSWORD_RABBITMQ, HOSTNAME_RABBITMQ, QUEUE } from '../config';
+import { PORT_RABBIT, USERNAME_RABBITMQ, PASSWORD_RABBITMQ, HOSTNAME_RABBITMQ, QUEUE } from '../config';
 
 const rabbitSettings = {
 	protocol: 'amqp',
 	hostname: HOSTNAME_RABBITMQ,
-	port: PORT_RABBITMQ,
+	port: PORT_RABBIT,
 	username: USERNAME_RABBITMQ,
 	password: PASSWORD_RABBITMQ,
 	authMechanism: ['PLAIN', 'AMQPLAIN', 'EXTERNAL']
